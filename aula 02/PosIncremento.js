da878f086b45bbc3824ac3573cbf67de9db91ff5
RAMONBRX <ramon.all98@hotmail.com>
2026-01-27 11:47:27 -0300

    Exemplos aula 2 e README adicionados

---
+++ aula 02/PosIncremento.js
@@ -0,0 +1,7 @@
+let numero = 1;
+
+console.log("valor atual: ",numero);
+
+console.log(`Valor da variável numero - Pós incrementada ${ numero ++}`);
+
+console.log(`Valor atualizado da variável numero ${numero}`);
---
+++ aula 02/Variaveis.js
@@ -0,0 +1,13 @@
+"use strict";
+let nome = 12;
+let profissao = "Pessoa Desenvolvedora";
+let bonus = 0.05;
+let salarioBruto = 3500;
+
+console.log("O tipo da variável nome é: ", typeof (nome));
+
+if (salarioBruto > 1500) {
+    let mensagem = "O salario bruto é R$ ";
+    console.log(mensagem + salarioBruto);
+}
+
